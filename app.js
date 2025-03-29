68d71c3a72ebe6f43db9237850f05ab7f2fa9b20
Aayush Aeron <aayushaeron13@gmail.com>
2025-03-29 21:41:42 +0530

    Add error.ejs to display handled errors to user

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.all("*",(req,res,next)=>{
 })
 app.use((err,req,res,next)=>{
      let {status=500,message="Something went wrong"}=err;
-    res.status(status).send(message);
+    res.status(status).render("error.ejs",{message});
 })
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
