1fdbcc984be05ddd58ef2d11c65a704c9d540bfd
Aayush Aeron <aayushaeron13@gmail.com>
2025-03-30 19:29:42 +0530

    Add Joi schema validation for listing object
    - Implement Joi schema validation for the listing object in server-side.
    - Ensure all required fields are validated before saving to database.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-
+const {listingSchema}=require("./schema.js");
 main()
     .then(() => console.log("Connected to DB"))
     .catch(err => console.log(err));
@@ -25,6 +25,14 @@ app.use(express.static(path.join(__dirname,"/public")))
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(400,result.error);
+     }
+     else
+     next();
+};
 // index route
 app.get("/listings", wrapAsync(async (req, res) => {
         const allListings = await Listing.find({});
@@ -43,11 +51,8 @@ app.get("/listings/:id",wrapAsync(async (req,res)=>{
     res.render("listings/show.ejs",{listing});
 }));
 // create 
-app.post("/listings", 
+app.post("/listings", validateListing,
     wrapAsync(async (req, res,next) => {
-        if(!req.body.listing){
-            throw new ExpressError(400,"Send valid data for listing");
-         }
         const newListing = new Listing(req.body.listing);
         await newListing.save(); 
         res.redirect("/listings"); 
@@ -58,10 +63,7 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
 }));
-app.put("/listings/:id",wrapAsync(async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send valid data for listing");
-     }
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
      let {id}=req.params;
      await Listing.findByIdAndUpdate(id,{...req.body.listing});
      res.redirect(`/listings/${id}`);
