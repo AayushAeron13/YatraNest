af255a8a3fcbffb13153e5a41c64ef9855b8f98f
Aayush Aeron <aayushaeron13@gmail.com>
2025-03-31 19:37:43 +0530

    feat: Add server side and client side validation for review form
    - Add joi validation for review form

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Listing = require("./Models/listings"); 
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
-const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
-const Review = require("./Models/review"); 
+const Listing = require("./Models/listings");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./Models/review");
 
 main()
     .then(() => console.log("Connected to DB"))
@@ -17,81 +17,90 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/YatraNest");
 }
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-     }
-     else
-     next();
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else
+        next();
+};
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else
+        next();
 };
 // index route
 app.get("/listings", wrapAsync(async (req, res) => {
-        const allListings = await Listing.find({});
-        res.render("listings/index.ejs", { allListings }); 
-    
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+
 }));
 
 // new route
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 // show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
 }));
 // create 
 app.post("/listings", validateListing,
-    wrapAsync(async (req, res,next) => {
+    wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
-        await newListing.save(); 
-        res.redirect("/listings"); 
-})
+        await newListing.save();
+        res.redirect("/listings");
+    })
 );
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
 }));
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-     let {id}=req.params;
-     await Listing.findByIdAndUpdate(id,{...req.body.listing});
-     res.redirect(`/listings/${id}`);
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
 }));
 // delete
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{    
-    let {id}=req.params;
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 }));
 // review post
-app.post("/listings/:id/reviews",async(req,res)=>{
-    let listing=await Listing.findById(req.params.id); // params is used when to access data from route/URL In this case ID is present
-    let newReview=new Review(req.body.review); // body is used when we post data In our case it is review
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id); // params is used when to access data from route/URL In this case ID is present
+    let newReview = new Review(req.body.review); // body is used when we post data In our case it is review
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
     res.redirect(`/listings/${listing._id}`);
+}));
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
 })
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"Page not found"));
-})
-app.use((err,req,res,next)=>{
-     let {status=500,message="Something went wrong"}=err;
-    res.status(status).render("error.ejs",{message});
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Something went wrong" } = err;
+    res.status(status).render("error.ejs", { message });
 })
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
