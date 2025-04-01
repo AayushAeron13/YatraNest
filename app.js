fbdd565bbaa748185f8563981113c7e5876fd20b
Aayush Aeron <aayushaeron13@gmail.com>
2025-04-01 14:14:13 +0530

    refactor: use Express Router for /listings routes
    - Refactored and organized routes under /listings
    - Improved code readability and modularity

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./Models/review");
+const listings =require("./routes/listing.js");
 
 main()
     .then(() => console.log("Connected to DB"))
@@ -17,6 +18,7 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/YatraNest");
 }
+
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -27,15 +29,9 @@ app.use(express.static(path.join(__dirname, "/public")))
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    else
-        next();
-};
+// wrapAsync helps karta hai agar koi error us taks ko karne mein aata hai toh woh 
+//error handling middlware ke pass chala jaye kyunk asunchronous next ko call nhi karte automatically
+
 const validateReview = (req, res, next) => {
     let { error } = reviewSchema.validate(req.body);
     if (error) {
@@ -45,47 +41,7 @@ const validateReview = (req, res, next) => {
     else
         next();
 };
-// index route
-app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-
-}));
-
-// new route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
-// show route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
-}));
-// create 
-app.post("/listings", validateListing,
-    wrapAsync(async (req, res, next) => {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
-);
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
-app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-}));
-// delete
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
+app.use("/listings",listings)
 // review post
 app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id); // params is used when to access data from route/URL In this case ID is present
---
+++ routes/listing.js
@@ -0,0 +1,61 @@
+ const express = require('express');
+ const router = express.Router();
+ const Listing = require('../Models/listings.js');
+ const wrapAsync =  require('../utils/wrapAsync.js');
+ const ExpressError = require('../utils/ExpressError.js');
+ const {listingSchema} = require('../schema.js');
+ //schema validation 
+ validateListing=function(req,res,next){
+     let {error}=listingSchema.validate(req.body);  // yeh hai joi wala agar hoppscotch se bhejte hai toh post request bina kuch entries dale 
+     if(error){
+         let errMsg=error.details.map((el)=>el.message).join(",");
+         throw new ExpressError(400,errMsg);
+     }else{
+         next();
+     }
+ }
+ 
+ router.get("/", wrapAsync(async (req, res) => {
+     const allListings = await Listing.find({});
+     res.render("listings/index.ejs", { allListings });
+ 
+ }));
+ 
+ // new route
+ router.get("/new", (req, res) => {
+     res.render("listings/new.ejs");
+ });
+ // show route
+ router.get("/:id", wrapAsync(async (req, res) => {
+     let { id } = req.params;
+     const listing = await Listing.findById(id).populate("reviews");
+     res.render("listings/show.ejs", { listing });
+ }));
+ // create 
+ router.post("/", validateListing,
+     wrapAsync(async (req, res, next) => {
+         const newListing = new Listing(req.body.listing);
+         await newListing.save();
+         res.redirect("/listings");
+     })
+ );
+ //edit
+ router.get("/:id/edit", wrapAsync(async (req, res) => {
+     let { id } = req.params;
+     const listing = await Listing.findById(id);
+     res.render("listings/edit.ejs", { listing });
+ }));
+ //update
+ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+     let { id } = req.params;
+     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+     res.redirect(`/listings/${id}`);
+ }));
+ // delete
+ router.delete("/listings/:id", wrapAsync(async (req, res) => {
+     let { id } = req.params;
+     await Listing.findByIdAndDelete(id);
+     res.redirect("/listings");
+ }));
+ 
+ module.exports = router;
