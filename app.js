3fc6e99472c612b2bd3745f75aa36a56c14800af
Aayush Aeron <aayushaeron13@gmail.com>
2025-04-02 16:08:54 +0530

    feat: Add connect-flash for success messages

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings =require("./routes/listing.js");
-const reviews=require("./routes/review.js");
-const session=require("express-session");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 main()
     .then(() => console.log("Connected to DB"))
     .catch(err => console.log(err));
@@ -23,23 +24,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")))
 
-const sessionOptions={
-    secret:"mysupersecretcode",
-    resave:false, // agar memory store mein change nhi hua ho phir bhi save karta tha band kar diya
-    saveUninitialized:true, //=>Agar koi naya session create hota hai lekin usme abhi tak koi data save nahi hua, 
-// tab bhi usko store karne ki permission deta hai. 
-     cookie:{
-       expires:Date.now()+7*24*3600*1000,
-       maxAge:7*24*3600*1000,
-       httpOnly:true,
-     },
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false, // agar memory store mein change nhi hua ho phir bhi save karta tha band kar diya
+    saveUninitialized: true, //=>Agar koi naya session create hota hai lekin usme abhi tak koi data save nahi hua, 
+    // tab bhi usko store karne ki permission deta hai. 
+    cookie: {
+        expires: Date.now() + 7 * 24 * 3600 * 1000,
+        maxAge: 7 * 24 * 3600 * 1000,
+        httpOnly: true,// cross site scripting attacks
+    },
 }
-app.use(session(sessionOptions));
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use(session(sessionOptions));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    next();
+});
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not found"));
 })
