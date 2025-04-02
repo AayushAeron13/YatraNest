bd8a7ad4437e4ac8869be6c4e57252d2844ad508
Aayush Aeron <aayushaeron13@gmail.com>
2025-04-02 15:38:49 +0530

    feat: Implement sessions with custom options

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const listings =require("./routes/listing.js");
 const reviews=require("./routes/review.js");
+const session=require("express-session");
 main()
     .then(() => console.log("Connected to DB"))
     .catch(err => console.log(err));
@@ -22,6 +23,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")))
 
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false, // agar memory store mein change nhi hua ho phir bhi save karta tha band kar diya
+    saveUninitialized:true, //=>Agar koi naya session create hota hai lekin usme abhi tak koi data save nahi hua, 
+// tab bhi usko store karne ki permission deta hai. 
+     cookie:{
+       expires:Date.now()+7*24*3600*1000,
+       maxAge:7*24*3600*1000,
+       httpOnly:true,
+     },
+}
+app.use(session(sessionOptions));
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
