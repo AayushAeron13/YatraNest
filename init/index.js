1803f9dd3739073630c8b3d8cb294081d22708db
Aayush Aeron <aayushaeron13@gmail.com>
2025-04-04 18:35:47 +0530

    chore: Add owner property to listing schema
    - Reinitialize database with updated schema

--- init/index.js
+++ init/index.js
@@ -8,8 +8,14 @@ main()
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/YatraNest');
 }
+getRandomOwner = function () {
+    let owners = ["67ee6a0d94e18cf170669dd7", "67efcca00b8a27ce4ad9077f", "67ee8cec40e20d150f2515d6", "67efd19ef2a9a392ddcba8cf"];
+    return owners[Math.floor(Math.random() * (4))];
+}
 const initDB = async () => {
     try {
+        await Listing.deleteMany({});
+        initData.data = initData.data.map((obj) => ({ ...obj, owner: getRandomOwner() }));
         await Listing.insertMany(initData.data);
         console.log("Data was initialized successfully!");
     } catch (err) {
