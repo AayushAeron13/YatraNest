de32e8537ab63c8ba81c531251b54487aae520ed
Aayush Aeron <aayushaeron13@gmail.com>
2025-04-05 12:03:29 +0530

    feat: Add author to review prior saving the review

--- routes/review.js
+++ routes/review.js
@@ -9,6 +9,7 @@
 router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id); // params is used when to access data from route/URL In this case ID is present
     let newReview = new Review(req.body.review); // body is used when we post data In our case it is review
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
